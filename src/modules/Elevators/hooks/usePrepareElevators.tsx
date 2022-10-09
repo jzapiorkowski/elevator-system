c2c29fb41a4db095b1a2c1c02f5b6f86105d7b3b
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-10-10 01:29:24 +0200

    adjust system's UI based on users choice

--- src/modules/Elevators/hooks/usePrepareElevators.tsx
+++ src/modules/Elevators/hooks/usePrepareElevators.tsx
@@ -1,9 +1,16 @@
 import { ElevatorShaft } from 'modules/ElevatorShaft';
+import { useSelector } from 'react-redux';
+import { StateType } from 'store';
+import { ElevatorSystem } from 'utils/ElevatorSystem/ElevatorSystem';
 
 export function usePrepareElevators() {
-  const elevators = Array.from(Array(4).keys()).map((index) => (
-    <ElevatorShaft elevatorIndex={index} />
-  ));
+  const system = useSelector<StateType, ElevatorSystem>(
+    (state) => state.ElevatorSystem.system
+  );
+
+  const elevators = Array.from(Array(system.getNumberOfElevators).keys()).map(
+    (index) => <ElevatorShaft elevatorIndex={index} />
+  );
 
   return { elevators };
 }
