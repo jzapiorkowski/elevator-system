02bb64e0a30d99059826593997ef34505686ec87
jzapiorkowski <92112490+jzapiorkowski@users.noreply.github.com>
2022-10-10 01:44:21 +0200

    fix: adjust number of elevator buttons (#12)

--- src/hooks/usePrepareElevatorButtons.tsx
+++ src/hooks/usePrepareElevatorButtons.tsx
@@ -1,13 +1,20 @@
 import { ElevatorCallButton } from 'modules/ElevatorCallButton';
 import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { StateType } from 'store';
+import { ElevatorSystem } from 'utils/ElevatorSystem/ElevatorSystem';
 
 export default function usePrepareElevatorButtons() {
+  const system = useSelector<StateType, ElevatorSystem>(
+    (state) => state.ElevatorSystem.system
+  );
+
   const elevatorButtons = useMemo(
     () =>
-      Array.from(Array(6).keys())
+      Array.from(Array(system.getNumberOfFloors).keys())
         .map((level) => <ElevatorCallButton level={level + 1} />)
         .reverse(),
-    []
+    [system.getNumberOfFloors]
   );
 
   return { elevatorButtons };
