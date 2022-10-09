3298e594caa340b483a0a93a49abd166757c2bd7
jzapiorkowski <92112490+jzapiorkowski@users.noreply.github.com>
2022-10-10 01:30:05 +0200

    feat: let user change system data (#7)
    * create form
    
    * adjust system's UI based on users choice

--- src/modules/ElevatorShaft/hooks/usePrepareElevatorShaft.tsx
+++ src/modules/ElevatorShaft/hooks/usePrepareElevatorShaft.tsx
@@ -1,5 +1,8 @@
 import { useMemo } from 'react';
 import { ElevatorLevel } from '../ElevatorLevel';
+import { useSelector } from 'react-redux';
+import { StateType } from 'store';
+import { ElevatorSystem } from 'utils/ElevatorSystem/ElevatorSystem';
 
 interface usePrepareElevatorShaftProps {
   elevatorIndex: number;
@@ -8,14 +11,18 @@ interface usePrepareElevatorShaftProps {
 export function usePrepareElevatorShaft({
   elevatorIndex,
 }: usePrepareElevatorShaftProps) {
+  const system = useSelector<StateType, ElevatorSystem>(
+    (state) => state.ElevatorSystem.system
+  );
+
   const shaft = useMemo(
     () =>
-      Array.from(Array(6).keys())
+      Array.from(Array(system.getNumberOfFloors).keys())
         .map((level) => (
           <ElevatorLevel level={level + 1} elevatorIndex={elevatorIndex} />
         ))
         .reverse(),
-    [elevatorIndex]
+    [elevatorIndex, system.getNumberOfFloors]
   );
 
   return { shaft };
